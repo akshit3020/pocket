8de0f85ef3f5b60ee4efaf05b409b9fce2ec4b2e
akshit3020 <akshit2953@zohomail.in>
2026-02-17 20:06:10 +0000

    setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="bg-red-500"
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
